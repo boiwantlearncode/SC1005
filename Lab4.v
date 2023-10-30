fda7bed05a4ff9425fe47d789cbd93aed869d933
Isa Bin Mohamed Yamin <22657943+boiwantlearncode@users.noreply.github.com>
2023-10-30 15:42:15 +0800

    Update Lab4.v

--- Lab4.v
+++ Lab4.v
@@ -51,17 +51,3 @@ module vaddoflow(
     
     assign oflow = sum[4];
 endmodule
-
-// Put in Lab5_top.v
-module Lab5_top(input clk, rst, sel, output [6:0] seg_L, output [3:0] anode_L);
-
-    // declare necessary wires here
-    wire adjusted_clk;
-    wire [15:0] value;
-
-    // instantiate modules here
-    scroll uut1 (.clk(adjusted_clk), .rst(rst), .display(value));
-    slow_clkgen uut2 (.clk(clk), .rst(rst), .clk_out(adjusted_clk));
-    seg7_driver uut3 (.clk(clk), .rst(rst), .sel(sel), .value(value), .anode_d(4'b0000), .seg_L(seg_L), .anode_L(anode_L));
-
-endmodule
